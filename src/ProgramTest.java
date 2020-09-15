770e6fb2735b54c2521e528670807265b1738e2e
Charles <charles.kaeme@gmail.com>
2020-09-15 12:31:26 -0600

    completed unit tests for getFile(). closes #6

--- src/ProgramTest.java
+++ src/ProgramTest.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramTest {
@@ -20,5 +22,24 @@ class ProgramTest {
 
     @org.junit.jupiter.api.Test
     void getFile() {
+        File testFileOne = new File("programs-test-file.csv");
+        boolean testCaseOne = testFileOne.exists();
+        assertEquals(false, testCaseOne);
+
+        File testFileTwo = new File("programs_test_file.txt");
+        boolean testCaseTwo = testFileTwo.exists();
+        assertEquals(false, testCaseTwo);
+
+        File testFileThree = new File("programs_test_file.xls");
+        boolean testCaseThree = testFileThree.exists();
+        assertEquals(false, testCaseThree);
+
+        File testFileFour = new File("program_test_file.csv");
+        boolean testCaseFour = testFileFour.exists();
+        assertEquals(false, testCaseFour);
+
+        File testFileFive = new File("programs_test_file.csv");
+        boolean testCaseFive = testFileFive.exists();
+        assertEquals(true, testCaseFive);
     }
 }
