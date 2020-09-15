2b059fdd571cebf330e2088c8d5f7c6d1bd4e8dc
Charles <charles.kaeme@gmail.com>
2020-09-15 12:20:33 -0600

    Added JUnit. closes #7

---
+++ src/ProgramTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pullPrograms() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addProgram() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteProgram() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFile() {
+    }
+}
